e4c9087414fe85971a353c73a39f83493d5c43d3
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-08-18 13:50:34 +0100

    implemented jquery and discussed vegetables

--- src/app.js
+++ src/app.js
@@ -4,58 +4,39 @@ TempColour = function() {
   temperature.style.color = thermostat.energyUsage();
 }
 
-var temperature = document.getElementById("temperature");
-  temperature.innerHTML = thermostat.showTemp();
-  TempColour();
-
-// var upTemperature = document.getElementById("increase");
-//   upTemperature.onclick = function() {
-//     thermostat.increaseTemp();
-//     temperature.innerHTML = thermostat.showTemp();
-//     TempColour();
-//   }
-
 $(document).ready(function() {
+  $("#temperature").show(function() {
+    temperature.innerHTML = thermostat.showTemp();
+    TempColour();
+  })
   $("#increase").click(function() {
     thermostat.increaseTemp();
     temperature.innerHTML = thermostat.showTemp();
     TempColour();
   })
-})
-
-$(document).ready(function() {
   $("#decrease").click(function() {
     thermostat.decreaseTemp();
     temperature.innerHTML = thermostat.showTemp();
     TempColour();
   })
-})
-
-
-// var downTemperature = document.getElementById("decrease");
-//   downTemperature.onclick = function() {
-//     thermostat.decreaseTemp();
-//     temperature.innerHTML = thermostat.showTemp();
-//     TempColour();
-//   }
-
-var resetTemperature = document.getElementById("reset");
-  resetTemperature.onclick = function() {
+  $("#reset").click(function() {
     thermostat.resetTemp();
     temperature.innerHTML = thermostat.showTemp();
     TempColour();
+  })
+  $('#power_save').click(function() {
+    powerSaveChange();
+  })
+  
+  function powerSaveChange() {
+    if ($("#power_save").is(':checked') ) {
+      thermostat.powerSaveOn();
+      temperature.innerHTML = thermostat.showTemp();
+    } else {
+      thermostat.powerSaveOff();
+    }
   }
-
-function powerSaveChange() {
-  if (document.getElementById("power_save").checked){
-    thermostat.powerSaveOn();
-    temperature.innerHTML = thermostat.showTemp();
-  } else {
-    thermostat.powerSaveOff();
-  }
-}
-
-
+})
 
 
  
