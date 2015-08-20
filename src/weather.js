8c00b1f2043d0ef46d38004e090c94cd61ac5c12
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-20 16:38:06 +0100

    rounded temp to 2 integers

--- src/weather.js
+++ src/weather.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $.getJSON("http://api.openweathermap.org/data/2.5/find?q=" + loc + "&units=metric",function(result){
       weather = result;
       alert("City: "+result.list[0].name);
-      alert("Weather: "+ result.list[0].main.temp);
+      alert("Weather: "+ Math.floor(result.list[0].main.temp));
       });
     });
 });
