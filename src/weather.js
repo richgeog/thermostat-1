aba2078f1652a3c82627476791972acb3553483b
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-20 16:24:11 +0100

    CSS improved and weather api now implemented and working

--- src/weather.js
+++ src/weather.js
@@ -1,9 +1,20 @@
 var weather;
 
-$(document).ready(function(){
-  $.getJSON("https://api.openweathermap.org/data/2.5/find?q=London&units=metric",function(result){
-    // weather = result;
-    alert("City: "+result.city.name);
-    alert("Weather: "+ result.main.temp);
+// $(document).ready(function(){
+//   $.getJSON("http://api.openweathermap.org/data/2.5/find?q=London&units=metric",function(result){
+//     weather = result;
+//     alert("City: "+result.list[0].name);
+//     alert("Weather: "+ result.list[0].main.temp);
+//     });
+// });
+
+$(document).ready(function() {
+  $('.locbtn').click(function() {
+    var loc = $('.city').val();
+    $.getJSON("http://api.openweathermap.org/data/2.5/find?q=" + loc + "&units=metric",function(result){
+      weather = result;
+      alert("City: "+result.list[0].name);
+      alert("Weather: "+ result.list[0].main.temp);
+      });
     });
 });
